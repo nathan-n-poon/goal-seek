8f6588fa12c41d5b01f73c5a930588dacf7c2be4
Nathan Poon <nathannpoon@gmail.com>
2020-05-26 20:45:16 +0000

    off by 0.0009

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <cmath>
 #define precision 0.000001
-#define goal 42500/15069
+#define goal 42500/15069.04
 #define power 4
 using namespace std;
 
 
 bool equivalent(double result)
 {
-  return abs(result - (double)goal) < 1.9*precision;
+  return abs(result - (double)goal) < 2.0*precision;
 }
 
 double result(double interest)
 {
-  return (pow((1+interest),4)-1) / (interest*pow((1+interest),power));
+  return (pow((1+interest),power)-1.0) / (interest*pow((1.0+interest),power));
 }
 
 
